1aa69f4fe703e25fa1fc139727448b9954d6d9c4
dominickbrasileiro <brasileiro.dominick@gmail.com>
2023-02-08 04:17:58 -0300

    chore(nest-backend): add mongodb to docker-compose.yml

---
+++ nest-backend/.docker/mongo/init.js
@@ -0,0 +1,20 @@
+db.routes.insertMany([
+  {
+    _id: "1",
+    title: "Route A",
+    startPosition: { lat: -15.82594, long: -47.92923 },
+    endPosition: { lat: -15.82942, long: -47.92765 },
+  },
+  {
+    _id: "2",
+    title: "Route B",
+    startPosition: { lat: -15.82449, long: -47.92756 },
+    endPosition: { lat: -15.8276, long: -47.92621 },
+  },
+  {
+    _id: "3",
+    title: "Route C",
+    startPosition: { lat: -15.82331, long: -47.92588 },
+    endPosition: { lat: -15.82758, long: -47.92532 },
+  },
+]);
