90894e12179d29b3358c2538ba50a9cec97c220a
dominickbrasileiro <brasileiro.dominick@gmail.com>
2023-02-10 04:33:29 -0300

    feat(nest-backend): add route id to response

--- nest-backend/src/routes/entities/route.entity.ts
+++ nest-backend/src/routes/entities/route.entity.ts
@@ -1,9 +1,22 @@
 import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
 export type RouteDocument = Route & Document;
 
-@Schema()
+@Schema({
+  toJSON: {
+    transform: function (_, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+    },
+  },
+})
 export class Route {
+  id: string;
+
+  @Prop()
+  _id: string;
+
   @Prop()
   title: string;
 
