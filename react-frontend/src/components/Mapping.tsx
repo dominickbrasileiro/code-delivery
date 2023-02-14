c5c085df555fdcd78946426a696538fdf823beb0
dominickbrasileiro <brasileiro.dominick@gmail.com>
2023-02-14 01:12:44 -0300

    feat(react-frontend): add google maps visualization

--- react-frontend/src/components/Mapping.tsx
+++ react-frontend/src/components/Mapping.tsx
@@ -1,10 +1,16 @@
 import { Button, FormControl, Grid, InputLabel, MenuItem, Select } from "@material-ui/core";
-import { FormEvent, useCallback, useEffect, useState } from "react";
+import { Loader } from "google-maps";
+import { FormEvent, useCallback, useEffect, useRef, useState } from "react";
+import { getCurrentPosition } from "../utils/geolocation";
 import { Route } from "../utils/models";
+
+const googleMapsLoader = new Loader(process.env.REACT_APP_GOOGLE_API_KEY)
 
 export const Mapping = () => {
   const [routes, setRoutes] = useState<Route[]>();
   const [selectedRouteId, setSelectedRouteId] = useState<string>("");
+
+  const mapRef = useRef<google.maps.Map>()
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/routes`, {
@@ -14,13 +20,38 @@ export const Mapping = () => {
       .then(json => setRoutes(json))
   }, []);
 
+  useEffect(() => {
+    (async () => {
+      const [, position] = await Promise.all([
+        googleMapsLoader.load(),
+        getCurrentPosition({ enableHighAccuracy: true }),
+      ])
+
+      const $map = document.getElementById("map") as HTMLElement
+
+      mapRef.current = new google.maps.Map($map, {
+        zoom: 15,
+        center: {
+          lat: position.lat,
+          lng: position.long,
+        },
+      })
+    })()
+  })
+
   const startRoute = useCallback((e: FormEvent) => {
     e.preventDefault()
     console.log(`Starting route ${selectedRouteId}...`)
   }, [selectedRouteId])
 
   return (
-    <Grid container >
+    <Grid
+      container
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    >
       <Grid item xs={12} sm={3}>
         <FormControl
           fullWidth
@@ -54,7 +85,13 @@ export const Mapping = () => {
         </FormControl>
       </Grid>
       <Grid item xs={12} sm={9}>
-        <div id="map" />
+        <div
+          id="map"
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
       </Grid>
     </Grid>
   );
