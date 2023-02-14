211ffb1d5aaed538af155ad2a2e3431543f4afff
dominickbrasileiro <brasileiro.dominick@gmail.com>
2023-02-14 01:11:25 -0300

    feat(react-frontend): add getCurrentPosition util

---
+++ react-frontend/src/utils/geolocation.ts
@@ -0,0 +1,14 @@
+export const getCurrentPosition = (
+  options?: PositionOptions
+): Promise<{ lat: number; long: number }> => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => resolve({
+        lat: position.coords.latitude,
+        long: position.coords.longitude,
+      }),
+      error => reject(error),
+      options,
+    )
+  })
+}
